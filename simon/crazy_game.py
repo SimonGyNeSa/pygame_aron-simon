2abe1af3f67c079e39e991da818250aa5aab9c22
SimonGyNeSa <simon.l5@gynesa.schule.koeln>
2025-05-20 18:41:51 +0200

    Add files via upload
    neues spiel konzept 
    altes war nicht gut in pygame umsetzbar
    auswahl von fruchten und auswahl vom ort wo sie spawnen
    warscheinlichketen für früchte festgelet
    'blitet' jetzt schon die füchte

--- simon/crazy_game.py
+++ simon/crazy_game.py
@@ -2,25 +2,33 @@ import pygame, time
 import random
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-zhal = randint(1,4)
+
 pygame.init()
 
 screensize = (1420, 720)
 WHITE = (255,255,255)
 BLACK = (0, 0, 0)
 
+number = 0
+
 screen = pygame.display.set_mode(screensize)
-
-asset = pygame.image.load("mein_asset_v2.jpg").convert_alpha()
-assetx, assety = 450, 100
-
-asset2 = pygame.image.load("mein_asset_v2.jpg").convert_alpha()
-asset2x, asset2y = 850, 100
-
-asset3 = pygame.image.load("mein_asset_v2.jpg").convert_alpha()
-asset3x, asset3y = 650, 100
-
 clock = pygame.time.Clock()
+
+# lädt die füchte
+bild1 = pygame.image.load("pic1.png") #Erdbeere
+bild2 = pygame.image.load("pic2.png") #Kirsche
+bild3 = pygame.image.load("pic3.png") #Blaubeere
+bild4 = pygame.image.load("pic4.png") #Anannas
+bild5 = pygame.image.load("pic5.png") #Apfel
+bild6 = pygame.image.load("pic6.png") #Kiwi 
+
+# beispiels-koordinaten wo die frucht spawnen kann
+spawn_orte = [
+    (10,10), (10,20), (10,30)
+    (20,10), (20,20), (20,30)
+    (30,10), (30,20), (30,30)
+]
+
 
 running = True
 while running:
@@ -28,25 +36,37 @@ while running:
         if event.type == pygame.QUIT:
             running = False 
         print(event)
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+            #stoppt nach 9 früchten
+                while number <= 9:
+                # wählt aus, welche Frucht spawnt
+                    zahl = random.randint(0, 36)
 
-    assety += 1
-    asset2y += 1
-    asset3y += 1
+                    if 0 <= zahl <= 8:
+                        furcht_die_spawnt = bild1
+                    elif 9 <= zahl <= 18:
+                        furcht_die_spawnt = bild2
+                    elif 19 <= zahl <= 28:
+                        furcht_die_spawnt = bild3
+                    elif 29 <= zahl <= 34:
+                        furcht_die_spawnt = bild4
+                    elif 35 <= zahl <= 35:
+                        furcht_die_spawnt = bild5
+                    elif zahl == 36:
+                        furcht_die_spawnt = bild6
 
-    if assety >= screensize[1]-20:
-        assety -= 720
+                    # wählt aus, wo die Frucht spawnt
+                    if spawn_orte:  # Nur wenn noch Orte übrig sind
+                        ausgewählt = random.choice(spawn_orte)
+                        spawn_orte.remove(ausgewählt)
+                        screen.blit(furcht_die_spawnt, ausgewählt)
 
-    if asset2y >= screensize[1]-20:
-        asset2y -= 720
+                    number += 1  # nicht vergessen, sonst Endlosschleife
 
-    if asset3y >= screensize[1]-20:
-        asset3y -= 720
 
     screen.fill(BLACK)
-    screen.blit(asset, (assetx, assety))
-    screen.blit(asset2, (asset2x, asset2y))
-    screen.blit(asset3, (asset3x, asset3y))
-
     pygame.display.flip()
 
 clock.tick(30)
