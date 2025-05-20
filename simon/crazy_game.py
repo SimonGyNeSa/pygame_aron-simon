77d516da62eeea31d4236b121b61cf645f856612
SimonGyNeSa <simon.l5@gynesa.schule.koeln>
2025-05-20 09:22:39 +0200

    Add files via upload
    erster entwurf casino mechanic

--- simon/crazy_game.py
+++ simon/crazy_game.py
@@ -1,7 +1,8 @@
 import pygame, time
+import random
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
+zhal = randint(1,4)
 pygame.init()
 
 screensize = (1420, 720)
@@ -10,6 +11,14 @@ BLACK = (0, 0, 0)
 
 screen = pygame.display.set_mode(screensize)
 
+asset = pygame.image.load("mein_asset_v2.jpg").convert_alpha()
+assetx, assety = 450, 100
+
+asset2 = pygame.image.load("mein_asset_v2.jpg").convert_alpha()
+asset2x, asset2y = 850, 100
+
+asset3 = pygame.image.load("mein_asset_v2.jpg").convert_alpha()
+asset3x, asset3y = 650, 100
 
 clock = pygame.time.Clock()
 
@@ -20,13 +29,23 @@ while running:
             running = False 
         print(event)
 
+    assety += 1
+    asset2y += 1
+    asset3y += 1
+
+    if assety >= screensize[1]-20:
+        assety -= 720
+
+    if asset2y >= screensize[1]-20:
+        asset2y -= 720
+
+    if asset3y >= screensize[1]-20:
+        asset3y -= 720
+
     screen.fill(BLACK)
-
-    rect = pygame.Rect(350, 300, 750, 350)
-    pygame.draw.rect(screen, WHITE, rect, 6, 1)
-
-    pygame.draw.line(screen, WHITE, (200, 100), (200, 500), 5)
-
+    screen.blit(asset, (assetx, assety))
+    screen.blit(asset2, (asset2x, asset2y))
+    screen.blit(asset3, (asset3x, asset3y))
 
     pygame.display.flip()
 
