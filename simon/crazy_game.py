42c9055bcdfb45648e3bcc6566eead90fd1c32cc
SimonGyNeSa <simon.l5@gynesa.schule.koeln>
2025-06-12 12:13:03 +0200

    Update crazy_game.py
    man kann mehrmals glücksspiel betreiben

--- simon/crazy_game.py
+++ simon/crazy_game.py
@@ -1,28 +1,25 @@
-import pygame, time
-import random
-import os
+import pygame, random, os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 pygame.init()
-
-screensize = (1420, 720)
-WHITE = (255,255,255)
+screensize = (600, 600)
 BLACK = (0, 0, 0)
 
-fruits = []
+ 
 
-number = 1
+spawn_orte = [
+    (170, 150), (270, 150), (370, 150),
+    (170, 250), (270, 250), (370, 250),
+    (170, 350), (270, 350), (370, 350)
+]
 
-screen = pygame.display.set_mode(screensize)
-clock = pygame.time.Clock()
-
-# lädt die füchte
-bild1 = pygame.image.load("strawberry.png") #Erdbeere
-bild2 = pygame.image.load("lemon.png") #Kirsche
-bild3 = pygame.image.load("plum.png") #Blaubeere
-bild4 = pygame.image.load("banana.png") #Anannas
-bild5 = pygame.image.load("watermelon.png") #Apfel
-bild6 = pygame.image.load("coconut.png") #Kiwi 
+# Früchte laden und skalieren
+bild1 = pygame.image.load("strawberry.png")
+bild2 = pygame.image.load("lemon.png")
+bild3 = pygame.image.load("plum.png")
+bild4 = pygame.image.load("banana.png")
+bild5 = pygame.image.load("watermelon.png")
+bild6 = pygame.image.load("coconut.png")
 
 scaled_bild1 = pygame.transform.scale(bild1, (70, 70))
 scaled_bild2 = pygame.transform.scale(bild2, (70, 70))
@@ -31,80 +28,58 @@ scaled_bild4 = pygame.transform.scale(bild4, (70, 70))
 scaled_bild5 = pygame.transform.scale(bild5, (70, 70))
 scaled_bild6 = pygame.transform.scale(bild6, (70, 70))
 
-# beispiels-koordinaten wo die frucht spawnen kann
-spawn_orte = [
-    (100,100), (100,200), (100,300),
-    (200,100), (200,200), (200,300),
-    (300,100), (300,200), (300,300)
-]
+screen = pygame.display.set_mode(screensize)
+clock = pygame.time.Clock()
 
+fruits = []  # globale Liste der Früchte, die gezeichnet werden
+
+def spawn_fruits():
+    local_spawn_orte = spawn_orte.copy()
+    fruits_local = []
+    number = 0
+
+    while number < 9 and local_spawn_orte:
+        zahl = random.randint(0, 36)
+        if 0 <= zahl <= 8:
+            furcht_die_spawnt = scaled_bild1
+        elif 9 <= zahl <= 18:
+            furcht_die_spawnt = scaled_bild2
+        elif 19 <= zahl <= 28:
+            furcht_die_spawnt = scaled_bild3
+        elif 29 <= zahl <= 34:
+            furcht_die_spawnt = scaled_bild4
+        elif 35 <= zahl <= 35:
+            furcht_die_spawnt = scaled_bild5
+        elif zahl == 36:
+            furcht_die_spawnt = scaled_bild6
+        
+        ausgewählt = random.choice(local_spawn_orte)
+        local_spawn_orte.remove(ausgewählt)
+        fruits_local.append((furcht_die_spawnt, ausgewählt))
+
+        number += 1
+    return fruits_local
 
 running = True
 while running:
+    screen.fill(BLACK)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False 
-        print(event)
+            running = False
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-            #stoppt nach 9 früchten
-                
-                while number <= 9:
-                # wählt aus, welche Frucht spawnt
-                    zahl = random.randint(0, 36)
+                fruits = spawn_fruits()  # neue Früchte generieren
 
-                    if 0 <= zahl <= 8:
-                        furcht_die_spawnt = scaled_bild1
-                    elif 9 <= zahl <= 18:
-                        furcht_die_spawnt = scaled_bild2
-                    elif 19 <= zahl <= 28:
-                        furcht_die_spawnt = scaled_bild3
-                    elif 29 <= zahl <= 34:
-                        furcht_die_spawnt = scaled_bild4
-                    elif 35 <= zahl <= 35:
-                        furcht_die_spawnt = scaled_bild5
-                    elif zahl == 36:
-                        furcht_die_spawnt = scaled_bild6
+    # Früchte zeichnen
+    for fruit, pos in fruits:
+        screen.blit(fruit, pos)
 
-                    # wählt aus, wo die Frucht spawnt
-                    if spawn_orte:  # Nur wenn noch Orte übrig sind
-                        ausgewählt = random.choice(spawn_orte)
-                        spawn_orte.remove(ausgewählt)
-                        fruits.append((furcht_die_spawnt, ausgewählt))
+    pygame.display.flip()
+    clock.tick(30)
 
-                    number += 1  # nicht vergessen, sonst Endlosschleife
+pygame.quit()
 
-                    if event.key == pygame.K_SPACE:
-                        #stoppt nach 9 früchten
-                        screen.fill(BLACK)
-                        while number <= 9:
-                        # wählt aus, welche Frucht spawnt
-                            zahl = random.randint(0, 36)
+import pygame
 
-                            if 0 <= zahl <= 8:
-                                furcht_die_spawnt = scaled_bild1
-                            elif 9 <= zahl <= 18:
-                                furcht_die_spawnt = scaled_bild2
-                            elif 19 <= zahl <= 28:
-                                furcht_die_spawnt = scaled_bild3
-                            elif 29 <= zahl <= 34:
-                                furcht_die_spawnt = scaled_bild4
-                            elif 35 <= zahl <= 35:
-                                furcht_die_spawnt = scaled_bild5
-                            elif zahl == 36:
-                                furcht_die_spawnt = scaled_bild6
-
-                            # wählt aus, wo die Frucht spawnt
-                            if spawn_orte:  # Nur wenn noch Orte übrig sind
-                                ausgewählt = random.choice(spawn_orte)
-                                spawn_orte.remove(ausgewählt)
-                                fruits.append((furcht_die_spawnt, ausgewählt))
-
-                            number += 1  # nicht vergessen, sonst Endlosschleife
-
-        screen.fill(BLACK)
-        for fruit, position in fruits:
-            screen.blit(fruit, position)
-        pygame.display.flip()
-        clock.tick(30)
